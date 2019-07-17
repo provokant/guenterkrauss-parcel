30cc5c502c6948d94b624a6b13117739bb06d876
Michael Czechowski <mail@dailysh.it>
2019-07-17 04:14:05 +0200

    added carousel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,54 @@
-import events from './events';
 import  './styles/index.scss';
+import Swiper from 'swiper'
 
-events();
+new Swiper ('#creations-2019', {
+  slidesPerView: 'auto',
+  // centeredSlides: true,
+  spaceBetween: 0,
+  // freeMode: true,
+  loop: true,
+  centeredSlides: true,
+  speed: 900,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+new Swiper ('#creations-2018', {
+  slidesPerView: 'auto',
+  // centeredSlides: true,
+  spaceBetween: 0,
+  // freeMode: true,
+  loop: true,
+  centeredSlides: true,
+  speed: 900,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
