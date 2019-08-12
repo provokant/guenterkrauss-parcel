487d7e5e8954e5f3400987fe84159ffa54aa65a8
Michael Czechowski <mail@dailysh.it>
2019-08-12 15:47:18 +0200

    added swipe effect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-import  './styles/index.scss';
+import  './styles/index.scss'
+
+const canvas = document.querySelector('header')
+const wrapper = document.querySelector('.swiper-wrapper')
+const width = canvas.offsetWidth
+const children = wrapper.children
+const activeClass = '--active'
+
+let activePosition = 0
+
+canvas.addEventListener('mousemove', (e) => {
+  const currentPosition = position(e)
+  updateActive(currentPosition)
+})
+
+function position(e) {
+  return Math.floor(e.pageX / width * children.length)
+}
+
+function updateActive(currentPosition) {
+  if (currentPosition === activePosition) return
+
+  children[activePosition].className = ''
+  children[currentPosition].className = activeClass
+
+  activePosition = currentPosition
+}
