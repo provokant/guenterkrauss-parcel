5780960e8352a1ff76bb283658ab389569cb3744
Michael Czechowski <mail@dailysh.it>
2019-08-13 12:07:18 +0200

    fixed tailwind config and adjusted fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@ module.exports = {
   theme: {
     extend: {},
     container: {
-      // center: true,
-      // padding: '2rem',
+      center: true,
+      padding: '2rem',
     },
+    fontSize: {
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.5rem',
+      '2xl': '6rem'
+    }
   },
   variants: {
     textColor: ['hover'],
