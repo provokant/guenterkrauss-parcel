7cbe154017fe5edc45d1b74d8e1cc04d83ac4cb0
Michael Czechowski <mail@dailysh.it>
2019-09-08 17:37:33 +0200

    classified swiper module

--- src/scripts/Swiper.ts
+++ src/scripts/Swiper.ts
@@ -1,47 +1,78 @@
 export default class Swiper {
 
-  private canvas
-  private wrapper
-  private children
-  private activeClass
   private width
-  private activePosition
+  private currentPosition
+  private options: Options
 
-  constructor() {
-    console.log('test')
-    this.canvas = document.querySelector('header')
-    this.wrapper = document.querySelector('.swiper-wrapper')
-    this.children = this.wrapper.children
-    this.activeClass = '--active'
+  constructor(options?: any) {
+    this.resetOptions(options)
     this.width = this.updateWidth()
-    this.activePosition = Math.floor(Math.random() * this.children.length)
-  
-    this.updateClasses(this.activePosition)
-  
+    this.randomPosition()
+    this.updateClasses(this.currentPosition)
+    this.attachListener()
+  }
+
+  private randomPosition(): void {
+    const { children } = this.wrapper
+
+    this.currentPosition = Math.floor(Math.random() * children.length)
+  }
+
+  private attachListener(): void {
     this.canvas.addEventListener('mousemove', e => {
       const nextPosition = this.position(e)
       this.updateActive(nextPosition)
     })
   }
 
+  private resetOptions(options: any): void {
+    this.options = {
+      ...{
+        canvasSelector: 'header',
+        wrapperSelector: '.swiper-wrapper',
+        childClass: '--active'
+      },
+      ...options
+    }
+  }
+
+  private get wrapper(): HTMLElement {
+    return document.querySelector(this.options.wrapperSelector)
+  }
+
+  private get canvas(): HTMLElement {
+    return document.querySelector(this.options.canvasSelector)
+  }
+
   private position(e): number {
-    return Math.floor(e.pageX / this.width * this.children.length)
+    const { children } = this.wrapper
+
+    return Math.floor(e.pageX / this.width * children.length)
   }
 
   private updateActive(nextPosition): void {
-    if (nextPosition === this.activePosition) return
+    if (nextPosition === this.currentPosition) return
 
     this.updateClasses(nextPosition)
 
-    this.activePosition = nextPosition
+    this.currentPosition = nextPosition
   }
 
   private updateClasses(position): void {
-    this.children[this.activePosition].classList.remove(this.activeClass)
-    this.children[position].classList.add(this.activeClass)
+    const { children } = this.wrapper
+    const { childClass } = this.options
+
+    children[this.currentPosition].classList.remove(childClass)
+    children[position].classList.add(childClass)
   }
 
   private updateWidth(): number {
     return this.width = this.canvas.offsetWidth
   }
+}
+
+interface Options {
+  canvasSelector: string 
+  wrapperSelector: string
+  childClass: string
 }
